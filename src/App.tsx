687f2ba264b373cbd723b440dcbf8cf668f0c5d0
Peterson Munuera <peterson.munuera@gmail.com>
2024-05-24 15:24:32 -0300

    feat: global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-export function App() {
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
+export function App() {
   return (
-    <h1>Hello world</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello world</h1>
+    </ThemeProvider>
   )
 }
